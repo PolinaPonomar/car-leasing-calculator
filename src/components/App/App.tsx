ad8c3beecff55b610453ac92323fb300fee08935
Polina Ponomareva <pp09117@gmail.com>
2022-10-02 17:38:37 +0300

    feat: make base logic of RangeSlider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import './App.scss'
 
 const App = () => {
+  const [price, setPrice] = useState(3300000);
+  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(Number(event.target.value))
+  }
+  
   return (
-    <h1 >
-      React is working
-    </h1>
+    <main className="page">
+      <h1 className="title">Рассчитайте стоимость автомобиля в лизинг</h1>
+      <div className="range-sliders">
+        <div className="range-slider">
+          <p className="range-slider__title"></p>
+          <div className="range-slider__body">
+            <input className="range-slider__price" type="number" id="rangeValue"></input>
+            <input className="range-slider__slider" type="range" min="1000000" max="6000000" defaultValue={price} onChange={handleNumberChange}></input>
+            <p>{price}</p>
+          </div>
+        </div>
+        <div className="range-slider"></div>
+        <div className="range-slider"></div>
+      </div>
+    </main>
   );
 };
 
